5fa8e088500342962b218f3abaeb5a2c0f45ab11
Gopikrishnan Srinivasan <gopi.s.krishnan@gmail.com>
2019-12-11 18:26:33 -0600

    Spring Initializer level - blank projects

---
+++ movie-info-service/src/test/java/io/gtech/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gtech.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
